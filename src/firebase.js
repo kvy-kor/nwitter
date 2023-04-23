9ec3fa7173f0d5b8b92b288310a264692d48e015
kvy-kor <bjyun30@gmail.com>
2023-04-23 18:17:18 +0900

    package-lock.json update

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCZ3V-AMEFAPw7SGf54vjGQO3fRuq6lxLY",
+    authDomain: "nwitter-13d8f.firebaseapp.com",
+    projectId: "nwitter-13d8f",
+    storageBucket: "nwitter-13d8f.appspot.com",
+    messagingSenderId: "609942055766",
+    appId: "1:609942055766:web:110b00f015876ad21004f7"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import firebase from "./App";
+console.log(firebase);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+//위 부분 유심히 볼 것
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
 );
 
